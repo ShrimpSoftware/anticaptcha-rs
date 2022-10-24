781968d95f6d492ab93400faf39cbd6bdfebe805
Grapphy <76534455+Grapphy@users.noreply.github.com>
2022-10-24 14:33:20 -0300

    error conversions

---
+++ src/errors.rs
@@ -0,0 +1,30 @@
+use crate::models::ErrorResponse;
+
+#[derive(Debug)]
+pub enum Error {
+    ApiError(ErrorResponse),
+    HTTPInternalError(String),
+}
+
+impl std::error::Error for Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Error::ApiError(resp) => write!(f, "Error response: {:?}", resp),
+            Error::HTTPInternalError(resp) => write!(f, "HTTP Request failed: {:?}", resp),
+        }
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(e: reqwest::Error) -> Self {
+        Error::HTTPInternalError(e.to_string())
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(_e: serde_json::Error) -> Self {
+        Error::HTTPInternalError(String::from("Invalid response"))
+    }
+}
