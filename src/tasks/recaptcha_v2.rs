729e84d5307026210392e9f62dd82b3cde18f5f3
Grapphy <76534455+Grapphy@users.noreply.github.com>
2022-10-29 13:41:10 -0300

    updating parameters

--- src/tasks/recaptcha_v2.rs
+++ src/tasks/recaptcha_v2.rs
@@ -5,27 +5,27 @@ pub struct RecaptchaV2Task {
     pub id: Option<i64>,
     pub website_url: String,
     pub website_key: String,
-    pub is_invisible: bool,
+    pub is_invisible: Option<bool>,
     pub data_s_value: Option<String>,
 }
 
 impl RecaptchaV2Task {
-    pub fn new(url: &str, site_key: &str) -> Self {
+    pub fn new(url: String, site_key: String) -> Self {
         RecaptchaV2Task {
             id: None,
-            website_url: String::from(url),
-            website_key: String::from(site_key),
-            is_invisible: true,
+            website_url: url,
+            website_key: site_key,
+            is_invisible: None,
             data_s_value: None,
         }
     }
 
-    pub fn set_sdata(&mut self, sdata: &str) {
-        self.data_s_value = Some(String::from(sdata))
+    pub fn set_sdata(&mut self, sdata: String) {
+        self.data_s_value = Some(sdata)
     }
 
     pub fn set_invisible(&mut self, is_invisible: bool) {
-        self.is_invisible = is_invisible;
+        self.is_invisible = Some(is_invisible);
     }
 }
 
@@ -41,18 +41,28 @@ impl Task for RecaptchaV2Task {
     }
 
     fn task_type(&self) -> String {
-        String::from("RecaptchaV2Task")
+        String::from("RecaptchaV2TaskProxyless")
     }
 
     fn description(&self) -> String {
-        String::from("Solve Google Recaptcha automatically")
+        String::from("Solve Google Recaptcha automatically (proxyless)")
     }
 
     fn into_map(&self) -> HashMap<&str, String> {
-        HashMap::from([
+        let mut map = HashMap::from([
             ("type", self.task_type()),
             ("websiteURL", self.website_url.clone()),
             ("websiteKey", self.website_key.clone()),
-        ])
+        ]);
+
+        if let Some(data_value) = &self.data_s_value {
+            map.insert("recaptchaDataSValue", data_value.clone());
+        }
+
+        if let Some(visibility) = &self.is_invisible {
+            map.insert("isInvisible", visibility.to_string());
+        }
+
+        map
     }
 }
