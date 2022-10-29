b86333468d1e404249e2be68464377b15e7a580c
Grapphy <76534455+Grapphy@users.noreply.github.com>
2022-10-29 17:34:18 -0300

    Serializables

--- src/models.rs
+++ src/models.rs
@@ -1,64 +1,12 @@
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-
-#[derive(Debug, Deserialize)]
-pub struct ErrorResponse {
-    #[serde(rename = "errorId")]
-    pub error_id: i8,
-    #[serde(rename = "errorCode")]
-    pub error_code: String,
-    #[serde(rename = "errorDescription")]
-    pub error_description: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct BalanceResponse {
-    pub balance: f64,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct RecaptchaV2Response {
-    #[serde(rename = "gRecaptchaResponse")]
-    pub grecaptcha: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct FunCaptchaResponse {
-    pub token: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct GeeTestResponse {
-    pub challenge: String,
-    pub validate: String,
-    pub seccode: String,
-}
+use serde::Serialize;
+use serde_json::Value;
 
 #[derive(Debug, Serialize)]
-pub struct APIRequest<'a> {
+pub struct APIRequest {
     #[serde(rename = "clientKey")]
     pub key: String,
-    pub task: Option<HashMap<&'a str, String>>,
-    #[serde(rename = "taskId")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub task: Option<Value>,
+    #[serde(rename = "taskId", skip_serializing_if = "Option::is_none")]
     pub task_id: Option<i64>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct TaskResponse {
-    #[serde(rename = "taskId")]
-    pub task_id: i64,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct TaskStatus<T> {
-    pub status: String,
-    pub solution: Option<T>,
-    pub cost: Option<String>,
-    pub ip: Option<String>,
-    #[serde(rename = "createTime")]
-    pub create_time: Option<i64>,
-    #[serde(rename = "endTime")]
-    pub end_time: Option<i64>,
-    #[serde(rename = "solveCount")]
-    pub solve_count: Option<i64>,
 }
