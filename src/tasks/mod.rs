12888707104f4b7ea3662aea033b9c83c8ff28d5
Grapphy <76534455+Grapphy@users.noreply.github.com>
2022-10-29 17:40:12 -0300

    function to make serde Value mergeable

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -1,5 +1,6 @@
+use crate::errors::Error;
 use serde::de::DeserializeOwned;
-use std::collections::HashMap;
+use serde_json::Value;
 
 pub mod recaptcha_v2;
 pub use recaptcha_v2::RecaptchaV2Task;
@@ -18,7 +19,22 @@ pub trait Task {
 
     fn get_task_id(&self) -> Option<i64>;
     fn set_task_id(&mut self, task_id: i64);
-    fn task_type(&self) -> String;
     fn description(&self) -> String;
-    fn into_map(&self) -> HashMap<&str, String>;
+    fn as_value(&self) -> Result<Value, Error>;
+}
+
+pub fn merge(a: &mut Value, b: Value) {
+    if let Value::Object(a) = a {
+        if let Value::Object(b) = b {
+            for (k, v) in b {
+                if v.is_null() {
+                    a.remove(&k);
+                } else {
+                    merge(a.entry(k).or_insert(Value::Null), v);
+                }
+            }
+            return;
+        }
+    }
+    *a = b;
 }
