92bd00f5f82650a1d4f8ce76491e27ea47aad579
Grapphy <76534455+Grapphy@users.noreply.github.com>
2022-10-29 17:39:49 -0300

    adding Proxiable trait to recaptcha

--- src/tasks/recaptcha_v2.rs
+++ src/tasks/recaptcha_v2.rs
@@ -1,22 +1,40 @@
-use crate::tasks::Task;
-use std::collections::HashMap;
+use crate::errors::Error;
+use crate::proxies::{Proxiable, Proxy};
+use crate::response::RecaptchaV2Response;
+use crate::tasks::{merge, Task};
 
+use serde::Serialize;
+use serde_json::Value;
+
+#[derive(Debug, Serialize, Default)]
 pub struct RecaptchaV2Task {
+    #[serde(skip_serializing)]
     pub id: Option<i64>,
+    #[serde(rename = "type")]
+    pub task_type: String,
+    #[serde(rename = "websiteURL")]
     pub website_url: String,
+    #[serde(rename = "websiteKey")]
     pub website_key: String,
+    #[serde(rename = "isInvisible")]
     pub is_invisible: Option<bool>,
+    #[serde(
+        skip_serializing_if = "Option::is_none",
+        rename = "recaptchaDataSValue"
+    )]
     pub data_s_value: Option<String>,
+    #[serde(skip_serializing)]
+    pub proxy: Option<Proxy>,
 }
 
 impl RecaptchaV2Task {
     pub fn new(url: String, site_key: String) -> Self {
         RecaptchaV2Task {
             id: None,
+            task_type: String::from("RecaptchaV2TaskProxyless"),
             website_url: url,
             website_key: site_key,
-            is_invisible: None,
-            data_s_value: None,
+            ..Default::default()
         }
     }
 
@@ -30,7 +48,7 @@ impl RecaptchaV2Task {
 }
 
 impl Task for RecaptchaV2Task {
-    type TaskResult = crate::models::RecaptchaV2Response;
+    type TaskResult = RecaptchaV2Response;
 
     fn get_task_id(&self) -> Option<i64> {
         self.id
@@ -40,29 +58,24 @@ impl Task for RecaptchaV2Task {
         self.id = Some(task_id)
     }
 
-    fn task_type(&self) -> String {
-        String::from("RecaptchaV2TaskProxyless")
-    }
-
     fn description(&self) -> String {
-        String::from("Solve Google Recaptcha automatically (proxyless)")
+        String::from("Solve Google Recaptcha automatically")
     }
 
-    fn into_map(&self) -> HashMap<&str, String> {
-        let mut map = HashMap::from([
-            ("type", self.task_type()),
-            ("websiteURL", self.website_url.clone()),
-            ("websiteKey", self.website_key.clone()),
-        ]);
-
-        if let Some(data_value) = &self.data_s_value {
-            map.insert("recaptchaDataSValue", data_value.clone());
+    fn as_value(&self) -> Result<Value, Error> {
+        if let Some(proxy) = &self.proxy {
+            let proxy_value = serde_json::to_value(proxy)?;
+            let mut task_value = serde_json::to_value(self)?;
+            merge(&mut task_value, proxy_value);
+            return Ok(task_value);
         }
+        Ok(serde_json::to_value(self)?)
+    }
+}
 
-        if let Some(visibility) = &self.is_invisible {
-            map.insert("isInvisible", visibility.to_string());
-        }
-
-        map
+impl Proxiable for RecaptchaV2Task {
+    fn set_proxy(&mut self, proxy: Proxy) {
+        self.proxy = Some(proxy);
+        self.task_type = String::from("RecaptchaV2Task");
     }
 }
